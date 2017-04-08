d6ff2f37b88617cc9d0b919e7512c0f2f7b2a976
Rosario Felix <elijan_11@hotmail.com>
2017-04-07 21:33:11 -0500

    implementand footer y mejoras

--- js/app.js
+++ js/app.js
@@ -28,5 +28,51 @@ window.addEventListener("scroll", function(){
 
 
 // coders con Javascript
+var chicasLaboratoria=["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado","Yessenia Burga",
+                  "Miriam Mendoza","Elizabeth Condori","Arantza Burga","Grecia Rayme",
+                  "Janine Vega","Rosario Felix","Daguiana Revoredo","Jenny Velasquez",
+                 "Nadia Cuadros","Michell More","Marilu Llamocca","Mariel Garcia",
+                 "Fiorella Cisneros","Geraldine Chauca","Yelitza Rivera","Stephanie Hiyagon",
+                 "Emma Tapia","Danna Franco","Flor Retamozo","Nathaly Pacheco",
+                 "Ericka Vidal","Katherine Ortega","Brilly Majuan","Flor Tello",
+                 "Leslie Avendaño", "Cindy Mendoza","Annia Flores","Betsi Loayza",
+                 "Aida Sulca","Milagros Gutierrez","Nakarid Jave","Angie Cóndor",
+                 "Maia Rojas","Ariana Cabana","Flor Condori","Mitch Rodríguez",
+                 "Naomi Villanueva","Mari Castillo","Miriam Peralta","Karin Alejo",
+                 "Liliana Peña","Ruth Salvador","Maribel Sevilla","Wendy Reyes",
+                 "Cinthia Stark","Maria Grecia Cutipa","Ana Durand","Glisse Jorge",
+                 "Neiza Nuñez","Sandra Solorzano"];
 
-};
+var  chicasLabo = document.getElementById("coders");
+chicasLabo.innerHTML="";
+  // se pone desde uno para la primera imagen
+	for(var i=1 ;i<=54;i++){
+
+    var creandoDiv =document.createElement('div');
+    creandoDiv.classList.add("grid");
+
+		var figure = document.createElement("figure");
+		figure.classList.add("chicas");
+
+		var creandoDivDos =document.createElement("div");
+		creandoDivDos.classList.add("overlay");
+
+		var img =document.createElement("img");
+		img.classList.add("img-box");
+
+		var figurita =document.createElement("figcaption");
+		figurita.classList.add("text");
+// se pone -1 por los arrays comienzan de cero
+		var nombre = document.createTextNode(chicasLaboratoria[i-1]);
+		img.setAttribute("src","img/students/"+ i+".png");
+
+						figurita.appendChild(nombre)
+						figure.appendChild(figurita);
+						figure.appendChild(img);
+  					figure.appendChild(creandoDivDos);
+  					creandoDiv.appendChild(figure);
+
+  					chicasLabo.appendChild(creandoDiv) ;
+		}
+
+  };
